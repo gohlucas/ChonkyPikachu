f9c47c47a0deda7325c142d8817bc45c70e2aa97
Gabriel Phua <gabrielphua@live.com>
2024-06-17 22:19:28 +0800

    Main Page Design Done

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,11 +2,13 @@
 import React from "react";
 import Link from "next/link";
 import StartEndForm from "../components/StartEndForm";
+import './mainpage.css';
 
 const Page = () => {
   return (
     <div className="page-container">
       <h1 className="page-title">ChonkyPikachu</h1>
+      <div className="page-subtitle">Select Start and End Rooms</div>
       <div className="page-form">
         <StartEndForm />
       </div>
